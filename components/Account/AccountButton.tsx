d2d88fdbeece621a563cd30651327af60d58a560
Noah Penza <penzanoah@gmail.com>
2023-11-10 08:47:39 +1100

    Init project, shadcn and layout

---
+++ components/Account/AccountButton.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+function AccountButton() {
+  return (
+    <div>
+        <Button>Log In</Button>
+    </div>
+  )
+}
+
+// AccountButton.propTypes = {}
+
+export default AccountButton
